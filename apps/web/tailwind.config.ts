65eec982857caeb08eb58195840fa35655fa1b3f
Justin Trenary <trenaryja@gmail.com>
2025-04-26 17:20:05 -0400

    scorched earch

--- apps/web/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import sharedConfig from 'tailwind-config/tailwind.config'
-import { Config } from 'tailwindcss'
-
-export default {
-  content: sharedConfig.content,
-  presets: [sharedConfig],
-} satisfies Config
--- packages/tailwind-config/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from 'tailwindcss'
-
-export default {
-  darkMode: 'class',
-  content: [`src/**/*.{js,ts,jsx,tsx}`],
-  theme: {},
-  plugins: [],
-} satisfies Config
